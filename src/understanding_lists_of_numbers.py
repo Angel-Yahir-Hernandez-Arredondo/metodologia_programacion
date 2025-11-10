bc47e4fb8fe34104fec1f99d24b4517f99840c96
Angel Yahir Hernandez Arredondo <yahirarre09@gmail.com>
2025-11-10 10:39:44 -0600

    Listas con numeros

--- src/understanding_lists_of_numbers.py
+++ src/understanding_lists_of_numbers.py
@@ -42,3 +42,12 @@ for value in range(1, 11):
     square = value ** 2
     squares.append(square)
 print("\nCuadrados de los primeros 10 numeros:", squares)
+
+## Mas metodos build in
+# Metodo min()
+digits = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
+print(min(digits))  # Salida: 0
+# Metodo max()
+print(max(digits))  # Salida: 9
+# Metodo sum()
+print(sum(digits))  # Salida: 45
