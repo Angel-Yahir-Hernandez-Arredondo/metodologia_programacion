fe17da25a1703705469bd582e9a54537a625620d
Angel Yahir Hernandez Arredondo <yahirarre09@gmail.com>
2025-11-10 10:36:45 -0600

    Listas numericas

---
+++ src/understanding_lists_of_numbers.py
@@ -0,0 +1,44 @@
+"""
+   Las listas tambien pueden almacenar numeros
+   y de hecho, son ideales para esto.
+   Python ofrece una gran cantidad de herramientas
+   que ayudan a trabajar eficazmente con listas de numeros.
+"""
+# Metodo build in range()
+"""
+   El metodo range() genera una serie de numeros.
+   Por ejemplo:
+"""
+print("Numeros del 0 al 9:")
+for value in range(10): # Genera numeros del 0 al 9
+    print(value)
+
+print("\nNumeros del 1 al 9:")
+for value in range(1, 10): # Genera numeros del 1 al 9
+    print(value)
+
+print("\nNumeros impares del 1 al 9:")
+for value in range(1, 10, 2): # Genera numeros impares del 1 al 9
+    print(value)
+odd_numbers = list(range(1, 10, 2))
+print("\nLista de numeros impares del 1 al 9:", odd_numbers)
+
+
+print("\nNumeros pares del 0 al 9:")
+for value in range(0, 10, 2): # Genera numeros pares del 0 al 9
+    print(value)
+even_numbers = list(range(0, 10, 2))
+print("\nLista de numeros pares del 0 al 9:", even_numbers)
+
+print("\nTabla de multiplicar del 9:")
+for value in range(0, 91, 9): # Genera la tabla de multiplicar del 9
+    print(value)
+multiples_of_nine = list(range(0, 91, 9))
+print("\nLista de multiplos de 9 del 0 al 90:", multiples_of_nine)
+
+# Cuadrados de los primeros 10 numeros
+squares = []
+for value in range(1, 11):
+    square = value ** 2
+    squares.append(square)
+print("\nCuadrados de los primeros 10 numeros:", squares)
