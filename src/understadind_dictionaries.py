2b310567cdd170f446846b68b20fc5cc6c5e605b
Angel Yahir Hernandez Arredondo <yahirarre09@gmail.com>
2025-11-24 10:40:24 -0600

    Diccionarios

--- src/understadind_dictionaries.py
+++ src/understadind_dictionaries.py
@@ -28,3 +28,34 @@ print(homer_0)
 marge["x-position"]=16
 marge["y-position"]=26
 marge["z-position"]=10
+
+alien_0={'color': 'yellow'}
+print(alien_0['color'])
+
+# Modifying values in a dictionary
+alien_0['color']='green'
+print(alien_0['color'])
+
+# Add elements to a dictionary
+alien_0['x-positions']=0
+alien_0['y-positions']=0
+alien_0['name']='Paul'
+
+print(alien_0)
+
+##Looping though items
+for key, value in alien_0.items():
+    print(f"\nKey: {key} has value: {value}")
+
+##Looping though keys
+for key in alien_0.keys():
+    print(f"\nKey: {key}")
+
+##Looping though values
+for value in alien_0.values():
+    print(f"\nValue: {value}")
+
+# NESTING
+# Listas de diccionarios
+# Listas en diccionario
+# Diccionarios en diccionarios
